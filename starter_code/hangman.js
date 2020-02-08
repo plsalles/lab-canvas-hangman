29b7a70eb0d8596dd3b95b7a3c1d3704fae22abf
Paulo Salles <plsalles@gmail.com>
2020-02-08 17:57:01 -0300

    WIP

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,39 +1,69 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ['teste','game','ironhack'];
+    this.secretWord = this.getWord(this.words);
+    this.letters = [];
+    this.guessedLetter = '';
+    this.errorsLeft = 10;
+  }
 
-//   }
+  getWord() {
+    
+    let randomIndex = Math.floor(Math.random() * this.words.length);
+    return this.words[randomIndex]; 
+  }
 
-//   getWord() {
+  checkIfLetter(keyCode) {
+    let string = String.fromCharCode(keyCode);
+    let alphabet = ['A','B','C','D','E','F','G','H','I','J','k','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    if(alphabet.includes(string.toUpperCase())){
+      return true;
+      } else {return false;}
+    
+  }
 
-//   }
+  checkClickedLetters(key) {
+    if(this.letters.length===0){
+      this.letters.push(key.toUpperCase());
+      return true;}
 
-//   checkIfLetter(keyCode) {
+    if (this.letters.includes(key.toUpperCase())){
+      return false;
+    } else {this.letters.push(key.toUpperCase());
+            return true;}
+    
+  }
 
-//   }
+  addCorrectLetter(i) {
 
-//   checkClickedLetters(key) {
+    this.guessedLetter += this.secretWord[i].toUpperCase();
 
-//   }
 
-//   addCorrectLetter(i) {
 
-//   }
+   
+  }
 
-//   addWrongLetter(letter) {
+  addWrongLetter(letter) {
+    if(!this.secretWord.toUpperCase().includes(letter.toUpperCase())){
+      this.errorsLeft --;
+    }
+  }
 
-//   }
+  checkGameOver() {
+    if(this.errorsLeft===0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-//   checkGameOver() {
+  checkWinner() {
+    this.
+  }
 
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
+}
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
